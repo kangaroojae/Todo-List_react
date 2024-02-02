ddd98ad2e4dc1eec942e1c31e50556f5727f77f1
Woojae Kang <woojaeka@buffalo.edu>
2024-02-01 22:25:10 -0500

    able to delete add tasks along with checkbox

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,21 @@
-import React from "react";
 import { useState } from "react";
+
 
 function Form(props) {
   const [name, setName] = useState("");
 
-  function handleSubmit(event){
-    event.preventDefault();
-    props.addTask("Say Hello!");
-
-  }
   function handleChange(event) {
-    console.log(event.target.value);
+    setName(event.target.value);
   }
-  
-  
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
+
   return (
-    <form>
-      <form onSubmit={handleSubmit}></form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
@@ -35,7 +34,6 @@ function Form(props) {
         Add
       </button>
     </form>
-    
   );
 }
 
